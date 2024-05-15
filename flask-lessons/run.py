75dc158e27dabae8686381ed540d78569a4bf144
Sean Tilson <seantilson@gmail.com>
2024-05-15 17:56:36 +0000

    more of flask walkthrough, doing advanced routing, breaking for tutoring

--- flask-lessons/run.py
+++ flask-lessons/run.py
@@ -17,6 +17,18 @@ def about():
     return render_template("about.html", page_title = "About", company = data)
 
 
+@app.route('/about/<member_name>')
+def about_member(member_name):
+    member_val = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj['url'] == member_name:
+                member_val = obj
+    return render_template("member.html",member=member_val)
+
+
+
 @app.route('/contact')
 def contact():
     return render_template("contact.html", page_title="Contact Us!")
