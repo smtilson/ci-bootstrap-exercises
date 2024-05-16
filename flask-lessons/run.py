5aa920d8b90cb33f40bb57c79a36670bc33fb68c
Sean Tilson <seantilson@gmail.com>
2024-05-16 12:47:05 +0000

    finish flask tutorial walkthrough, did not deploy to heroku

--- flask-lessons/run.py
+++ flask-lessons/run.py
@@ -1,46 +1,54 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 import json
 
-app = Flask(__name__)
+if os.path.exists("env.py"):
+    import env
 
-@app.route('/')
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+@app.route("/")
 def index():
     return render_template("index.html", page_title="")
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
     data = []
-    with open("data/company.json","r") as json_data:
+    with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title = "About", company = data)
+    return render_template("about.html", page_title="About", company=data)
 
 
-@app.route('/about/<member_name>')
+@app.route("/about/<member_name>")
 def about_member(member_name):
     member_val = {}
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
-            if obj['url'] == member_name:
+            if obj["url"] == member_name:
                 member_val = obj
-    return render_template("member.html",member=member_val)
+    return render_template("member.html", member=member_val)
 
 
-
-@app.route('/contact')
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method=="POST":
+        flash(f"Thanks {request.form.get('name')}, we have have received your"\
+              "message!")
     return render_template("contact.html", page_title="Contact Us!")
 
 
-@app.route('/careers')
+@app.route("/careers")
 def careers():
     return render_template("careers.html", page_title="Careers")
 
+
 if __name__ == "__main__":
     app.run(
-        host = os.environ.get("IP","0.0.0.0"),
-        port = int(os.environ.get("PORT","5000")),
-        debug = True
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True,
     )
